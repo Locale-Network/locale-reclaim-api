e8ea41cdcd1b88274df1c188f0e94fececeb2487
kev-techi <kevin@techi.be>
2024-08-16 08:29:27 +0200

    cors

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,7 @@ const nextConfig = {
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: "https://webapp-seven-chi.vercel.app",
+            value: "*",
           },
           {
             key: "Access-Control-Allow-Methods",
